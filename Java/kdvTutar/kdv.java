9982ac1b942a138dec43f9b830dcd51e97083a60
asafemreozturk <asafemre12@gmail.com>
2022-04-20 18:11:04 +0300

    Java

---
+++ Java/kdvTutar/kdv.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	double ucret,tutar, kdvOran=0.18;
+      
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Ücret tutarını girin: ");
+      ucret=sc.nextInt();
+      
+      double kdvTutar=ucret*kdvOran;
+      double kdvliTutar=ucret+kdvTutar;
+      System.out.print("Kdv dahil ücret: " + kdvliTutar + " TL");
+	}
+}
