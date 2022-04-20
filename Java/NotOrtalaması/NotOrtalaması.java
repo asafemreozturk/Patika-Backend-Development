c66c0c70a102f02dca437247fb68a9cfb9b855d7
asafemreozturk <asafemre12@gmail.com>
2022-04-20 17:57:33 +0300

    Create NotOrtalaması.java

---
+++ Java/NotOrtalaması/NotOrtalaması.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	int mat,fizik,turkce,tarih, biyoloji;
+      
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Mat notu girin: ");
+      mat=sc.nextInt();
+      
+      System.out.print("Fizik notu girin: ");
+      fizik=sc.nextInt();
+      
+      System.out.print("Turkce notu girin: ");
+      turkce=sc.nextInt();
+      
+      System.out.print("Tarih notu girin: ");
+      tarih=sc.nextInt();
+      
+      System.out.print("Biyoloji notu girin: ");
+      biyoloji=sc.nextInt();
+      
+      int sum=mat+fizik+turkce+tarih+biyoloji;
+      int ort=sum/5;
+      System.out.print("Ortalama Notunuz: "+ort);
+      
+     String result= (ort >60) ? " Sınıfı Gecti"  : " Sınıfta Kaldı";
+     System.out.println(result);
+	}
+}
