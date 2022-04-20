64f7d8c94954de24e75349f2c1c2ab014bed1cf0
asafemreozturk <asafemre12@gmail.com>
2022-04-20 18:13:56 +0300

    Not Ortalaması
    Not Ortalaması Programı

--- Java/NotOrtalaması/NotOrtalaması.java
+++ Java/NotOrtalaması/NotOrtalaması.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
 
 public class Main
 {
-	public static void main(String[] args) {
-	int mat,fizik,turkce,tarih, biyoloji;
+    public static void main(String[] args) {
+      int mat,fizik,turkce,tarih, biyoloji;
       
       Scanner sc=new Scanner(System.in);
       System.out.print("Mat notu girin: ");
@@ -32,7 +32,7 @@ public class Main
       int ort=sum/5;
       System.out.print("Ortalama Notunuz: "+ort);
       
-     String result= (ort >60) ? " Sınıfı Gecti"  : " Sınıfta Kaldı";
-     System.out.println(result);
-	}
+      String result= (ort >60) ? " Sınıfı Gecti"  : " Sınıfta Kaldı";
+      System.out.println(result);
+    }
 }
