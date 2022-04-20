0747faea36b2d30fdde243748999304dedc3e8ea
asafemreozturk <asafemre12@gmail.com>
2022-04-20 18:35:43 +0300

    Hipotenüs Hesabı
    Hipotenüs Hesabı

---
+++ Java/hipotenüsHesabı/pythagorean.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	int a,b;
+	double c;
+	Scanner sc=new Scanner(System.in);
+	System.out.print("1. kenarı giriniz: ");
+	a=sc.nextInt();
+	System.out.print("2. kenarı giriniz: ");
+	b=sc.nextInt();
+	
+	c=Math.sqrt(b*b+a*a);
+	System.out.print("Hipotenüs uzunluğu: "+c);
+	}
+}
